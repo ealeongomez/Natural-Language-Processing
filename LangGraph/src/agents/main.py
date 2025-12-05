de31191cd11cf17bdbca749e7ded5d94ec489960
ealeongomez <ealeongomez@gmail.com>
2025-12-05 14:43:40 -0500

    Utils

---
+++ LangGraph/src/agents/main.py
@@ -0,0 +1,26 @@
+# pip install -qU langchain "langchain[anthropic]"
+from langchain.agents import create_agent
+from langchain_openai import ChatOpenAI
+from pathlib import Path
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+def get_weather(city: str) -> str:
+    """Get weather for a given city."""
+    return f"It's always sunny in {city}!"
+
+# Initialize the model
+model = ChatOpenAI(model="gpt-4o-mini")
+
+agent = create_agent(
+    model=model,
+    tools=[get_weather],
+    system_prompt="You are a helpful assistant",
+)
+
+# Run the agent
+agent.invoke(
+    {"messages": [{"role": "user", "content": "what is the weather in sf"}]}
+)
